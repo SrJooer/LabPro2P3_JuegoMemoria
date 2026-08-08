376d1e9acec30bcaa58ff406b593f73242a9861c
Jhov12 <hossycarias@gmail.com>
2026-08-08 11:02:58 -0600

    Update GestorTurnos.java

--- src/Control/GestorTurnos.java
+++ src/Control/GestorTurnos.java
@@ -1,9 +1,5 @@
-package labpro2p3_juegomemoria.control;//Jhoveth MOncada
-
-public interface GestorTurnos{
-    
+package labpro2p3_juegomemoria.control; // Jhoveth Moncada 
+public interface GestorTurnos { 
     void CambiarTurno();
-    
     int getJugadorActual();
-    
 }
