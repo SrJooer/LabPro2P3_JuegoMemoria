b08183978b8172c2b551c30af9dbb2be3663ed73
Jhov12 <hossycarias@gmail.com>
2026-08-08 10:58:32 -0600

    Update package declaration in LogicaJuego.java

--- src/Control/LogicaJuego.java
+++ src/Control/LogicaJuego.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package labpro2p3_juegomemoria.control;
+package labpro2p3_juegomemoria.control;//Jhoveth Moncada
 
 import labpro2p3_juegomemoria.modelo.Carta;
 
